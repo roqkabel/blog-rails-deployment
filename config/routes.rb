ddf652d3335eb439cd348f526a111805b5b313d9
roqkabel <roqkabel@gmail.com>
2023-08-10 16:39:39 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  get 'pages/about' 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  
 
   root "pages#index"
 end
